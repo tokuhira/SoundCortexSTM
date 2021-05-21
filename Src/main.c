4aa970e3fa04780c753a4acbf2b73672b9bc0234
tokuhira <masatsune@tokuhira.net>
2021-05-22 02:18:09 +0900

    Sync base condition between F0 and G0
    No efect.

--- Src/main.c
+++ Src/main.c
@@ -286,7 +286,10 @@ void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
-
+  __disable_irq();
+  while (1)
+  {
+  }
   /* USER CODE END Error_Handler_Debug */
 }
 
